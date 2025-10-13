90bf23dab86c5b369cc9ae9839e3a579bd72fdd8
Roger Roca Arano <git@rogerroca.me>
2025-10-13 17:48:42 -0400

    feat: Añadir pantallas de navegación para Favoritos, Inicio, Pedidos y Productos
    Se añadieron pantallas de navegación como placeholder para facilitar el desarrollo de AppNavBar.
    
    closes #11

---
+++ app/src/main/java/me/rogerroca/supernow/features/orders/ui/OrdersScreen.kt
@@ -0,0 +1,19 @@
+package me.rogerroca.supernow.features.orders.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OrdersScreen() {
+    Column {
+        Text("Welcome to the Orders Screen!")
+    }
+}
+
+@Preview
+@Composable
+fun OrdersScreenPreview() {
+    OrdersScreen()
+}
