f79daa7ce07cd5310835f0ec1bdcc16264b72988
Roger Roca Arano <git@rogerroca.me>
2025-10-17 17:08:39 -0400

    feat: actualizar MarketCard para usar un icono vectorial desde los recursos de la app

--- app/src/main/java/me/rogerroca/supernow/core/ui/components/MarketCard.kt
+++ app/src/main/java/me/rogerroca/supernow/core/ui/components/MarketCard.kt
@@ -1,29 +1,38 @@
 package me.rogerroca.supernow.core.ui.components
-import me.rogerroca.supernow.R
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import me.rogerroca.supernow.core.ui.theme.AppTypography
-import androidx.compose.ui.res.painterResource
-import me.rogerroca.supernow.core.ui.theme.onPrimaryLight
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.remember
 import coil.compose.AsyncImage
+import me.rogerroca.supernow.R
+import me.rogerroca.supernow.core.ui.theme.AppTypography
+import me.rogerroca.supernow.core.ui.theme.onPrimaryLight
 
 @Composable
 fun MarketCard(
@@ -79,8 +88,10 @@ fun MarketCard(
                                 style = AppTypography.labelSmall
                             )
                             Spacer(modifier = Modifier.width(4.dp))
+
+                            val iconResId = R.drawable.icon_material_star
                             Icon(
-                                imageVector = Icons.Default.Star,
+                                imageVector = ImageVector.vectorResource(iconResId),
                                 contentDescription = "Calificación",
                                 modifier = Modifier.size(12.dp)
                             )
