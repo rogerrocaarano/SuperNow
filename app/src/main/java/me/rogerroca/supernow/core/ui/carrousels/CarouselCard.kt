d7d5d87a6fddc96137f9720bbae8870a7ec913c9
Roger Roca Arano <git@rogerroca.me>
2025-10-19 16:08:25 -0400

    feat: añadir soporte de texto a CarouselCard

--- app/src/main/java/me/rogerroca/supernow/core/ui/carrousels/CarouselCard.kt
+++ app/src/main/java/me/rogerroca/supernow/core/ui/carrousels/CarouselCard.kt
@@ -2,15 +2,21 @@ package me.rogerroca.supernow.core.ui.carrousels
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,7 +57,38 @@ fun CarouselCard(
                     )
                 }
             )
+            text?.let {
+                CardTextOverlay(text = it)
+            }
         }
+
+    }
+}
+
+@Composable
+fun BoxScope.CardTextOverlay(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.BottomStart)
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black.copy(alpha = 0.7f)
+                    )
+                )
+            )
+            .padding(16.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium,
+            color = Color.White
+        )
     }
 }
 
