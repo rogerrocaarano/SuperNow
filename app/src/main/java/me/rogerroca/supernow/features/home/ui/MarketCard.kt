b17e7330788c2941d922515790aa6efcc564cebb
Nath Ardaya <102395902+nathaliascode@users.noreply.github.com>
2025-10-20 03:17:12 -0400

    refactor(ui): actualizar MarketCard para usar MarketCardClass, ImageBox e InfoColumn

--- app/src/main/java/me/rogerroca/supernow/features/home/ui/MarketCard.kt
+++ app/src/main/java/me/rogerroca/supernow/features/home/ui/MarketCard.kt
@@ -1,21 +1,14 @@
 package me.rogerroca.supernow.features.home.ui
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,19 +16,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import me.rogerroca.supernow.R
 import me.rogerroca.supernow.core.ui.components.FavoriteButton
-import me.rogerroca.supernow.core.ui.theme.AppTypography
-import me.rogerroca.supernow.core.ui.theme.onPrimaryLight
+import me.rogerroca.supernow.core.ui.components.ImageBox
+import me.rogerroca.supernow.core.ui.components.InfoColumn
+import me.rogerroca.supernow.core.ui.components.MarketCardClass
 
-@Composable
+/*@Composable
 fun MarketCard(
     modifier: Modifier = Modifier,
     name: String,
@@ -119,6 +107,46 @@ fun MarketCard(
             }
         }
     }
+}*/
+
+@Composable
+fun MarketCard(
+    market: MarketCardClass,
+    modifier: Modifier = Modifier,
+    action: @Composable (() -> Unit)? = null
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .padding(8.dp),
+        shape = RoundedCornerShape(12.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ImageBox(imageUrl = market.imageUrl)
+
+            Row(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(horizontal = 16.dp)
+                    .weight(1f),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                InfoColumn(
+                    name = market.name,
+                    address = market.address,
+                    score = market.score,
+                    modifier = Modifier.weight(1f)
+                )
+
+                action?.invoke()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -130,11 +158,29 @@ fun MarketCardPreview() {
         var isFavB by remember { mutableStateOf(false) }
         var isFavC by remember { mutableStateOf(false) }
 
-        MarketCard(
+        val marketA = MarketCardClass(
             name = "Local A",
             address = "Av. Principal #123",
-            imageUrl = "https://via.placeholder.com/150",
-            score = 5,
+            imageUrl = "https://sofia.com.bo/cdn/shop/files/Logos-marcas_27fb5342-061a-4b42-8586-237f677d7c87.png?v=1752000122&width=3200",
+            score = 5
+        )
+
+        val marketB = MarketCardClass(
+            name = "Local B",
+            address = "Calle #45",
+            imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6C2MCmrk6t0Ncdxx0CbqwFJDtaUJ_hcKjew&s",
+            score = 4
+        )
+
+        val marketC = MarketCardClass(
+            name = "Local C",
+            address = "Calle #3",
+            imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1upn4Zx_VRjRfenZby2gMhIEtLDPMu3d5xQ&s",
+            score = 3
+        )
+
+        MarketCard(
+            market = marketA,
             action = {
                 FavoriteButton(
                     isFavorite = isFavA,
@@ -144,10 +190,7 @@ fun MarketCardPreview() {
         )
 
         MarketCard(
-            name = "Local B",
-            address = "Calle #45",
-            imageUrl = "https://via.placeholder.com/150",
-            score = 4,
+            market = marketB,
             action = {
                 FavoriteButton(
                     isFavorite = isFavB,
@@ -157,10 +200,7 @@ fun MarketCardPreview() {
         )
 
         MarketCard(
-            name = "Local C",
-            address = "Calle #3",
-            imageUrl = "https://via.placeholder.com/150",
-            score = 3,
+            market = marketC,
             action = {
                 FavoriteButton(
                     isFavorite = isFavC,
@@ -170,3 +210,4 @@ fun MarketCardPreview() {
         )
     }
 }
+
