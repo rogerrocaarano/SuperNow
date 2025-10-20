d176cdf1fc2cb585ec4a4efbcadeee31fcfb95e1
Nath Ardaya <102395902+nathaliascode@users.noreply.github.com>
2025-10-20 03:15:51 -0400

    feat(model): agregar MarketCardClass para encapsular datos de MarketCard

---
+++ app/src/main/java/me/rogerroca/supernow/core/ui/components/MarketCardClass.kt
@@ -0,0 +1,8 @@
+package me.rogerroca.supernow.core.ui.components
+
+data class MarketCardClass(
+    val name: String,
+    val address: String? = null,
+    val score: Int? = null,
+    val imageUrl: String? = null
+)
