df07bdef9b1aabc4fc6f51474204815339988e74
Nath Ardaya <102395902+nathaliascode@users.noreply.github.com>
2025-10-20 06:29:24 -0400

    refactor(ui): mover FavoriteButton y actualizar para usar icono filled

--- app/src/main/java/me/rogerroca/supernow/core/ui/buttons/FavoriteButton.kt
+++ app/src/main/java/me/rogerroca/supernow/core/ui/buttons/FavoriteButton.kt
@@ -1,35 +1,33 @@
-package me.rogerroca.supernow.core.ui.components
+package me.rogerroca.supernow.core.ui.buttons
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import me.rogerroca.supernow.core.ui.theme.onPrimaryContainerLight
 import me.rogerroca.supernow.core.ui.theme.primaryLight
 import me.rogerroca.supernow.core.ui.theme.secondaryLight
 import me.rogerroca.supernow.core.ui.theme.surfaceDimLight
+import me.rogerroca.supernow.R
 
 @Composable
 fun FavoriteButton(
     isFavorite: Boolean,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    size: Dp = 32.dp
 ) {
     IconButton(
         onClick = onClick,
         modifier = modifier
-            .size(32.dp)
+            .size(size)
             .border(
                 width = 1.dp,
                 color = if (isFavorite) primaryLight else surfaceDimLight,
@@ -38,10 +36,13 @@ fun FavoriteButton(
             .clip(CircleShape)
     ) {
         Icon(
-            imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+            painter = painterResource(
+                id = if (isFavorite) R.drawable.icon_material_favorite_filled
+                else R.drawable.icon_material_favorite
+            ),
             contentDescription = if (isFavorite) "Quitar de favoritos" else "Agregar a favoritos",
             tint = if (isFavorite) secondaryLight else onPrimaryContainerLight,
-            modifier = Modifier.size(20.dp)
+            modifier = Modifier.size(size * 0.625f)
         )
     }
 }
