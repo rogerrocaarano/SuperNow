3877ed033000c8ae6b9887d9d3bbb95edfbe7f27
Roger Roca Arano <git@rogerroca.me>
2025-10-21 12:49:56 -0400

    feat: añadir componente product card (#39)
    * refactor: eliminar imports innecesarios en ImageBox
    
    * refactor: eliminar parámetro imageSize de ImageBox y quitar import innecesario
    
    * feat: agregar ProductCardModel para representar datos de productos
    
    * feat: crear ProductCardContent con detalles del producto y botón de agregar al carrito
    
    * feat: crear ProductCard que combina imagen y ProductCardContent
    
    * refactor: eliminar funcion innecesaria ProductInfoColumn de ProductCardContent
    
    * refactor: quitar imports innecesarios de ProductCardContent
    
    ---------
    
    Co-authored-by: Nath Ardaya <102395902+nathaliascode@users.noreply.github.com>

--- app/src/main/java/me/rogerroca/supernow/core/ui/misc/ImageBox.kt
+++ app/src/main/java/me/rogerroca/supernow/core/ui/misc/ImageBox.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import me.rogerroca.supernow.core.ui.theme.primaryLight
