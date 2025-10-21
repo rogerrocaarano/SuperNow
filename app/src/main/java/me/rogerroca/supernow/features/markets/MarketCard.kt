b218863f9f9c02f36af58a32d54ebfffc379a0cc
Nath Ardaya <102395902+nathaliascode@users.noreply.github.com>
2025-10-21 05:33:23 -0400

    refactor: eliminar imports innecesarios en MarketCard

--- app/src/main/java/me/rogerroca/supernow/features/markets/MarketCard.kt
+++ app/src/main/java/me/rogerroca/supernow/features/markets/MarketCard.kt
@@ -1,18 +1,14 @@
 package me.rogerroca.supernow.features.markets
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
