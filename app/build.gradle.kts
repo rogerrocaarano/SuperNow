bdf992c0ba4a5548dd6a601e4a520e4e846ed9b2
Roger Roca Arano <git@rogerroca.me>
2025-10-20 22:51:19 -0400

    feat: añadir dependencia de la librería lorem en build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.google.fonts)
     implementation(libs.coil.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.lorem)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
