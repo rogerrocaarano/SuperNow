d2fd0e82180b0649c14feedd0cc53fcae5556bd1
Nath Ardaya <102395902+nathaliascode@users.noreply.github.com>
2025-10-21 05:32:54 -0400

    refactor: eliminar imports innecesarios en ImageBox

--- app/src/main/java/me/rogerroca/supernow/core/ui/misc/ImageBox.kt
+++ app/src/main/java/me/rogerroca/supernow/core/ui/misc/ImageBox.kt
@@ -1,9 +1,7 @@
 package me.rogerroca.supernow.core.ui.misc
-import android.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
