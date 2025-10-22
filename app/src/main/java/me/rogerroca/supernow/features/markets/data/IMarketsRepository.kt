2538de6ba1d3758d92b2f29971fce502b2896307
Roger Roca Arano <git@rogerroca.me>
2025-10-22 03:01:40 -0400

    feat: actualizar IMarketsRepository para usar funciones suspend

--- app/src/main/java/me/rogerroca/supernow/features/markets/data/IMarketsRepository.kt
+++ app/src/main/java/me/rogerroca/supernow/features/markets/data/IMarketsRepository.kt
@@ -2,9 +2,9 @@ package me.rogerroca.supernow.features.markets.data
 
 import me.rogerroca.supernow.core.ui.carrousels.CarouselCardModel
 import me.rogerroca.supernow.features.markets.model.Market
+import me.rogerroca.supernow.features.markets.model.Offer
 
 interface IMarketsRepository {
-    fun getMarkets(): List<Market>
-    fun getMarketById(id: String): Market?
-    fun getMarketOffers(): List<CarouselCardModel>
+    suspend fun getMarkets(): List<Market>
+    suspend fun getMarketOffers(): List<Offer>
 }
