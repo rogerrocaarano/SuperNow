c67536817e64d5ff8093884e76165bf4475b335e
Roger Roca Arano <git@rogerroca.me>
2025-10-23 09:59:18 -0400

    feat: añadir MarketsHeader para mostrar el encabezado de la pantalla de mercados

---
+++ app/src/main/java/me/rogerroca/supernow/features/markets/ui/MarketsHeader.kt
@@ -0,0 +1,15 @@
+package me.rogerroca.supernow.features.markets.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import me.rogerroca.supernow.core.ui.headers.ScreenHeader
+
+@Composable
+fun MarketsHeader(modifier: Modifier = Modifier) {
+    ScreenHeader(
+        modifier = modifier.padding(horizontal = 16.dp),
+        header = "Armemos tu pedido"
+    )
+}
